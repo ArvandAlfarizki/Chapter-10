880c6add7f8d1f37c5a9707f94362a95f8450514
ArvandAlfarizki <arvand.rizki01@gmail.com>
2021-10-08 16:26:44 +0700

    Problem-4

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    private Rigidbody2D rigidbody2D; 
+    
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+
+        rigidbody2D.velocity = direction * movementSpeed;
+    }
+}
