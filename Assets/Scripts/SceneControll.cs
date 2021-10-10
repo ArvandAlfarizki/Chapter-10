0a4a983f7fd5d51f3aa8f8c34614ed054a1753fc
ArvandAlfarizki <arvand.rizki01@gmail.com>
2021-10-10 14:22:34 +0700

    Problem-10

---
+++ Assets/Scripts/SceneControll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneControll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
